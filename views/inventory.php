0134a00e273f12e2fa36c17ea103f6a5b24ce428
Ionman64 <PeterPickerill2014@gmail.com>
2017-11-28 20:41:21 +0100

    fixed display issue in inventory where headers would wrap at small screen sizes

--- views/inventory.php
+++ views/inventory.php
@@ -1,18 +1,15 @@
 <section class="container-fluid">
 	<section class="row" style="border-bottom:2px solid #000">
-		<section class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-			
-		</section>
-		<section class="col-lg-6 col-md-6 col-sm-8 col-xs-6">
+		<section class="col-lg-5 col-md-5 col-sm-8 col-xs-6">
 			<h4>Name</h4>
 		</section>
-		<section class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
-			<h4>Max Stock<span class="sub-text italic"> (inc. Display)</span></h4>
+		<section class="col-lg-3 col-md-3 col-sm-2 col-xs-3">
+			<h4>Max Stock<span class="sub-text italic hidden-xs hidden-sm"> (inc. Display)</span></h4>
 		</section>
-		<section class="col-lg-2 col-md-2 col-sm-1 col-xs-2">
-			<h4>In Stock<span class="sub-text italic"> (inc. Display)</span></h4>
+		<section class="col-lg-3 col-md-3 col-sm-2 col-xs-3">
+			<h4>In Stock<span class="sub-text italic hidden-xs hidden-sm"> (inc. Display)</span></h4>
 		</section>
-		<section class="col-lg-1 col-md-1 col-sm-1 col-xs-1 hidden-xs">
+		<section class="col-lg-1 col-md-1 hidden-sm hidden-xs">
 			
 		</section>
 	</section>
