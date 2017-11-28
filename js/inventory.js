a6a6ad503e09e5e039692530b82d22f0e57c8efb
Ionman64 <PeterPickerill2014@gmail.com>
2017-11-28 20:43:07 +0100

    fixed display issue where headers were not lined up with rows

--- js/inventory.js
+++ js/inventory.js
@@ -35,32 +35,23 @@ function Inventory() {
 		$("#inventory-table").empty();
 		$.each(this.products, function(key, product) {
 			var row = el("section", {class:"row product"});
-			//bin
-			var section =  el("section", {class:"col-lg-1 col-md-1 col-sm-1 col-xs-1"});
-			var button = el("button", {class:"btn btn-danger btn-lg delete", "data-id":key});
-			button.onclick = function() {
-				getTransaction().removeProduct($(this).attr("productID"));
-			};
-			button.appendChild(el("i", {class:"fa fa-times"}));
-			section.appendChild(button);
-			row.appendChild(section);
 			//name
-			var section = el("section", {class:"col-lg-6 col-md-6 col-sm-8 col-xs-6"});
+			var section = el("section", {class:"col-lg-5 col-md-5 col-sm-8 col-xs-6"});
 			var button = el("button", {class:"btn btn-default btn-lg product-btn", html:product.name, "data-id":key});
 			section.appendChild(button);
 			row.appendChild(section);
 			//max stock
-			var section = el("section", {class:"col-lg-2 col-md-2 col-sm-1 col-xs-2"});
+			var section = el("section", {class:"col-lg-3 col-md-3 col-sm-2 col-xs-3"});
 			var p = el("h4", {class:"text-default clear-text", "data-id":key, html:product.max_stock});
 			section.appendChild(p);
 			row.appendChild(section);
 			//in stock 
-			var section = el("section", {class:"col-lg-2 col-md-2 col-sm-1 col-xs-2"});
+			var section = el("section", {class:"col-lg-3 col-md-3 col-sm-2 col-xs-3"});
 			var p = el("h4", {class:"text-default clear-text", "data-id":key, html:product.current_stock});
 			section.appendChild(p);
 			row.appendChild(section);
 			//Order
-			var section = el("section", {class:"col-lg-1 col-md-1 col-sm-1 col-xs-1 hidden-xs"});
+			var section = el("section", {class:"col-lg-1 col-md-1 hidden-sm hidden-xs"});
 			var button = el("button", {class:"btn btn-info btn-lg order", html:"Order", "data-id":key});
 			section.appendChild(button);
 			row.appendChild(section);
