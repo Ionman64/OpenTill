6a893c1f86417e45f22117525e49c820496a1c17
Ionman64 <PeterPickerill2014@gmail.com>
2017-12-18 16:31:14 +0100

    Added Java Server

---
+++ server/src/com/opentill/main/Application.java
@@ -0,0 +1,44 @@
+package com.opentill.main;
+
+//
+//========================================================================
+//Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
+//------------------------------------------------------------------------
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the Eclipse Public License v1.0
+//and Apache License v2.0 which accompanies this distribution.
+//
+//  The Eclipse Public License is available at
+//  http://www.eclipse.org/legal/epl-v10.html
+//
+//  The Apache License v2.0 is available at
+//  http://www.opensource.org/licenses/apache2.0.php
+//
+//You may elect to redistribute this code under either of these licenses.
+//========================================================================
+//
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+//import org.omg.CORBA.Request;
+
+public class Application extends AbstractHandler
+{
+	@Override
+	public void handle(String target, org.eclipse.jetty.server.Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		 // Declare response encoding and types
+	    response.setContentType("text/html; charset=utf-8");
+	    // Declare response status code
+	    response.setStatus(HttpServletResponse.SC_OK);
+	    // Write back response
+	    response.getWriter().println("<h1>Hello World</h1>");
+	    // Inform jetty that this request has now been handled
+	    baseRequest.setHandled(true);
+	}
+}
---
+++ server/src/com/opentill/main/Main.java
@@ -0,0 +1,26 @@
+package com.opentill.main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+
+import com.opentill.log.Log;
+
+public class Main {
+		public static final int PORT = 8080;
+		public static void main( String[] args ) throws Exception
+		{
+		    Server server = new Server();
+		    server.setStopAtShutdown(true);
+		    ServerConnector http = new ServerConnector(server);
+		    http.setHost("localhost");
+	        http.setPort(8080);
+	        http.setIdleTimeout(30000);
+	        // Set the connector
+	        server.addConnector(http);
+	        // Set a handler
+		    server.setHandler(new Application());
+		    server.start();
+		    Log.log("Server Started on PORT:" + PORT);
+		    server.join();
+		}
+}
